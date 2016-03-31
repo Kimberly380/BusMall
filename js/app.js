165fd3a28b6704961c9e36ad093e4cf871434476
Kim Feinberg <kimberly380@gmail.com>
2016-03-30 23:48:02 -0700

    most everything has been added.  Added 8-more button.  Added chart. Reset button having issues resetting the counters so game just continues.

--- js/app.js
+++ js/app.js
@@ -5,9 +5,12 @@ var allImages  = document.getElementsByClassName("allImages");
 var buttons    = document.getElementsByName("buttons");
 var listItems  = document.getElementById("listItems");
 
+var ctx = document.getElementById("resultsChart").getContext("2d"); //var for chart.js
+
 //***********Define Object Constructor and put all objects in array with extra array for file names only****************
 imgObjectsArray = [];
-imgFileArray=[];
+imgFileArray=[];  //for index location of clicked images
+imgNameArray=[];  //for graph labels
 
 function imgObjects (imgName, imgFile){
   this.imgName = imgName;
@@ -17,10 +20,13 @@ function imgObjects (imgName, imgFile){
 
   imgObjectsArray.push(this);
   imgFileArray.push(this.imgFile);
+  imgNameArray.push(this.imgName);
+
 }
 
 //*************image data ***********************************************************
 
+function makeImages () {
 var img1 = new imgObjects ("hotdog", "images/hotdog.jpg");
 var img2 = new imgObjects ("donuts", "images/donuts.jpg");
 var img3 = new imgObjects ("spaghetti", "images/spaghetti.jpg");
@@ -31,7 +37,8 @@ var img7 = new imgObjects ("pancakes", "images/pancakes.jpeg");
 var img8 = new imgObjects ("salad", "images/salad.jpg");
 var img9 = new imgObjects ("tacos", "images/tacos.jpg");
 var img10 = new imgObjects ("sushi", "images/sushi.jpg");
-
+}
+makeImages();
 
 //********Min Max function for random image selection **********
 
@@ -43,19 +50,20 @@ function randomSelector () {
 
 //++++++++++++++++++generate random image display+++++++++++++++++++++++++++++
 
-var imagesShown
-var imagesClicked=[];
-
 //function to generate images being seen (random) and counts # times shown.
 function showImage(){
     var randNum1 = randomSelector();
-    var randNum2 = randomSelector();
-    var randNum3 = randomSelector();
     imgHolder1.src = imgObjectsArray[randNum1].imgFile;
-    imgHolder2.src = imgObjectsArray[randNum2].imgFile;
-    imgHolder3.src = imgObjectsArray[randNum3].imgFile;
     imgObjectsArray[randNum1].showCount += 1;
+
+    do {var randNum2 = randomSelector();
+    }  while(randNum2 === randNum1);
+    imgHolder2.src = imgObjectsArray[randNum2].imgFile;
     imgObjectsArray[randNum2].showCount += 1;
+
+    do {var randNum3 = randomSelector();
+    }while(randNum3 === randNum2 && randNum3 ===randNum2);
+    imgHolder3.src = imgObjectsArray[randNum3].imgFile;
     imgObjectsArray[randNum3].showCount += 1;
 }
 
@@ -67,9 +75,12 @@ showImage();
 //**************************************
 
 //On click (of image), run this function, but not more than 16 times.
-var counter = 0;
+var maxClicks = 16;   //to set maximum number of clicks a user will be able to generate
+var counter = 0;   //to count number of clicks
+var extendPlay=false;
+
 function countClicks(){
-  if(counter < 16){
+  if(counter <maxClicks ){
       counter = counter + 1;
 
       var srcSelect = (this.src).split("/").pop();   //pull out file name from src path of clicked item
@@ -77,50 +88,85 @@ function countClicks(){
       imgObjectsArray[srcSelIndex].clickCount +=1;       //log click to object's counter
 
       showImage()
-    }else {   //display buttons and disable clicking.
+
+    }else if (counter===maxClicks && !extendPlay){
       buttons[0].style.display="inline";
       buttons[1].style.display="inline";
-      imgHolder3.disabled=true;
-    }
-}
 
-imgHolder1.addEventListener("click", countClicks, false);
+    } else if (counter === maxClicks && extendPlay){
+      buttons[0].style.display="inline";
+    }
+  }
+
+
+imgHolder1.addEventListener("click", countClicks , false);
 imgHolder2.addEventListener("click", countClicks, false);
 imgHolder3.addEventListener("click", countClicks, false);
 
 
-//################## click on button shows results ######################################
-var percentCalc //this is just a placeholder to avoid the divide by zero error...
-
+//################## click on button shows results #############################################################################
+var percentCalc; //this is just a placeholder to avoid the divide by zero error...(TODO: removed % of total temporarily.  Will put back in next pass).
+var resultsBarChart;  //put here so I can delete it later (delete happens outside of function.)
 function genResultsList (){
-  for(ii=0; ii<imgObjectsArray.length; ii++){
-    var imageResult = document.createElement("li");   //create list items
 
-              if(imgObjectsArray[ii].showCount === 0){   //I know this looks messy, but it avoids the NaN of divide by zero. not sure how to format.
-                percentCalc = 0;
-              }else {percentCalc = imgObjectsArray[ii].clickCount/imgObjectsArray[ii].showCount}
-
-    var imageResultText = document.createTextNode(imgObjectsArray[ii].imgName + " = " +imgObjectsArray[ii].showCount + " times shown and   "+imgObjectsArray[ii].clickCount + " times selected. That's " +Math.floor(percentCalc*100) +"%");  //create text for list items.
-    imageResult.appendChild(imageResultText);  //append text to list item
-    listItems.appendChild(imageResult);  //append list item to list
-  }
   buttons[0].style.display="none";
   buttons[1].style.display="none"
   buttons[2].style.display="block";
+
+
+var imgClickArray = [];
+for(var i = 0; i < imgObjectsArray.length; i++){
+  imgClickArray.push(imgObjectsArray[i].clickCount);
+}
+
+    var data = {
+        labels: imgNameArray,
+        datasets: [
+            {
+                label: "Images You Clicked",
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data: imgClickArray
+            }
+        ]
+    };
+      resultsBarChart = new Chart(ctx).Bar(data);
 }
 
 buttons[0].addEventListener("click", genResultsList, false);
 
+//######################## continue play button #################################
+
+function playMore () {
+      maxClicks=24;
+      extendPlay=true;
+      buttons[0].style.display="none";
+      buttons[2].style.display="none";
+      buttons[1].style.display="none";
+  }
+
+  buttons[1].addEventListener("click", playMore ,false);
 
 //################## reset button #########################################################################################
 //NOTE: functionality here is still buggy.  To be fixed!
 
 function reset(){
-    listItems.parentNode.removeChild(listItems);
-    imgHolder3.disabled=false;
+    resultsBarChart.destroy();
     buttons[0].style.display="none";
     buttons[2].style.display="none";
-    buttons[1].style.display="none"
+    buttons[1].style.display="none";
+    counter = 0;
+    maxClicks=16;
+    extendPlay=false;
+
+    for(var ii; ii < imgObjectsArray.length; ii++){
+        imgObjectsArray[ii].clickCount = 0;
+        imgObjectsArray[ii].showCount = 0;
+    }
+
+    showImage();
 }
 
 buttons[2].addEventListener("click", reset ,false);
